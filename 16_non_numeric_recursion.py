c5b3e5fa6f69dada3c06720835607b6048a6714e
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2025-02-13 09:37:44 +0530

    Add files via upload

---
+++ 16_non_numeric_recursion.py
@@ -0,0 +1,14 @@
+
+def fib_efficient(n, d):
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_efficient(n-1, d) + fib_efficient(n-2, d)
+        d[n] = ans
+        return ans
+
+d = {1:1, 2:1}
+# print(fib_efficient(34, d))
+
+print(fib_efficient(7, d))
+
