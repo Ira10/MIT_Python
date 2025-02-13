5a7997958ccc9fedea1278a2fa18e7421a8e605f
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2025-02-13 09:38:43 +0530

    Add files via upload

--- 16_non_numeric_recursion.py
+++ 16_non_numeric_recursion.py
@@ -1,3 +1,17 @@
+###############
+## Fibonacci without a dictionary 
+#################
+def fib_recur(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib_recur(n-1)+fib_recur(n-2)
+
+print(fib_recur(7))
+
+###############
+## Fibonacci with a dictionary 
+#################
 
 def fib_efficient(n, d):
     if n in d:
